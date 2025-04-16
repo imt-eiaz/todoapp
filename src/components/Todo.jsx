2e56c4deb2b0570fb228a3f4c50fb311425dd14a
imt-eiaz <imteiaz@gmail.com>
2025-04-16 17:28:08 +0100

    32

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,20 +2,42 @@ import { useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [myItem, setMyItem] = useState("");
+  const [myItems, setMyItems] = useState([]);
+  const [todos, setTodos] = useState([]);
+  let result = "";
+
+  function final() {
+    result("I will " + todo + " " + "and " + myItem);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
+    setTodos([...todos, todo]);
+    setMyItems([...myItems, myItem]);
   }
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input
           onChange={(e) => setTodo(e.target.value)}
-          value={todo}
           type="text"
+          value={todo}
+          placeholder="Enter todo here"
+        />
+        <input
+          onChange={(e) => setMyItem(e.target.value)}
+          type="text"
+          value={myItem}
+          placeholder="Enter myItem in Number here"
         />
         <button type="submit">Add</button>
       </form>
+      {console.log(
+        "I will " + todo + " pm" + " " + "and sleep " + myItem + "pm"
+      )}
+      {final}
     </div>
   );
 }
