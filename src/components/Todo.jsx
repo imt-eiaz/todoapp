d20ec90e49480b367ba7521e99681604f2fee520
imt-eiaz <imteiaz@gmail.com>
2025-04-18 17:38:28 +0100

    32 continue pratice

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,6 +4,7 @@ import TodoItem from "./TodoItem";
 export default function Todo() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...todos, todo]);
