c572f8e7f5d568a9feb01dca0bfc34a420d8a727
Imtiaz Laptop <imteiaz@gmail.com>
2025-04-19 18:46:35 +0100

    r

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import "./../App.css";
 import TodoItem from "./TodoItem";
-
 export default function Todo() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
-  function handleSubmit(e) {
+  function sub(e) {
     e.preventDefault();
     setTodos([...todos, todo]);
+    setTodo("");
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="todo">
+      <form onSubmit={sub}>
         <input
           type="text"
           value={todo}
@@ -19,7 +20,6 @@ export default function Todo() {
         />
         <button>Add</button>
       </form>
-
       {todos.map((item) => (
         <TodoItem item={item} key={item} />
       ))}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ import styles from "./todoitem.module.css";
 export default function TodoItem({ item }) {
   return (
     <>
-      <h3 className={styles.h3}>{item}</h3>
+      <h3>{item}</h3>
     </>
   );
 }
