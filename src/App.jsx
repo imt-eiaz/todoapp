9ffb72bccc25cc07a41467c221f5427b2821d03b
Imtiaz Laptop <imteiaz@gmail.com>
2025-04-20 14:42:41 +0100

    36 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header";
 import Todo from "./components/Todo";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div>
+        <Header />
         <Todo />
         {/* <Student name="Imtiaz" age={40} isReg={true} /> */}
       </div>
